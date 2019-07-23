fd5c3c5a09e77f4884d2f272df0a0303957ebb4b
lmk <lmk@ubuntu-server.lan>
2019-07-23 15:04:02 +0000

    go-vscode

---
+++ golang-c9/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## 关闭nginx的守护进程执行，防止docker启动后，自动停止的问题
+# echo "daemon off;" >> /etc/nginx/nginx.conf 
+#service php7.0-fpm start && nginx
+
+pm2 start /root/c9/server.js --no-daemon --name cloud9 -- --packed false -p 81 -l 0.0.0.0 -a ${C9_AUTH:-user:pass} -w /apps
+#packed 是压缩的意思 但是一旦压缩好像会出现编辑器无法编辑的问题，在谷歌浏览器里面。所以可以改成false
+
+
